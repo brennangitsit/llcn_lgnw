c6ddde0c109bd4c09e74f53cb13a0a14d02375de
Brennan Terhune <62226641+brennangitsit@users.noreply.github.com>
2024-11-07 15:09:07 -0800

    fix paths

--- 0_preprocessing/preprocess_r_c_s_n_s7.m
+++ 0_preprocessing/preprocess_r_c_s_n_s7.m
@@ -1,6 +1,6 @@
 % List of open inputs
 nrun = 1; % enter the number of runs here
-jobfile = {'/Users/bterhunecotter/My Drive/SDSU_LLCN/READ/2_Scripts/0_preprocessing/preprocess_r_c_s_n_s7_job.m'};
+jobfile = {'/Users/bterhunecotter/MyDrive/SDSU_LLCN/LGNW/2_Analysis/0_preprocessing/preprocess_r_c_s_n_s7_job.m'};
 jobs = repmat(jobfile, 1, nrun);
 inputs = cell(0, nrun);
 for crun = 1:nrun
